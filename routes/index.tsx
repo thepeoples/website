b2ee4be7fef08730cd7919f5323d06d12424dd03
Ross Peoples <ross.peoples@gmail.com>
2023-03-29 11:19:49 -0500

    Temporarily disable redirects

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,16 +11,16 @@ const LOCALHOST = "localhost:8000";
 // App entrypoint
 export const handler: Handlers<Page> = {
     async GET(_req, ctx) {
-        // redirect HTTP requests to HTTPS
-        if (_req.url.startsWith(HTTP_PREFIX + LOCALHOST)) {
-            console.log('Skipping redirect to HTTP since running locally');
-        } else if (_req.url.startsWith(HTTP_PREFIX)) {
-            const uri = _req.url.slice(HTTP_PREFIX.length);
-            return new Response(undefined, {
-                status: 301,
-                headers: {location: `${HTTPS_PREFIX}${uri}`}
-            });
-        }
+        // // redirect HTTP requests to HTTPS
+        // if (_req.url.startsWith(HTTP_PREFIX + LOCALHOST)) {
+        //     console.log('Skipping redirect to HTTP since running locally');
+        // } else if (_req.url.startsWith(HTTP_PREFIX)) {
+        //     const uri = _req.url.slice(HTTP_PREFIX.length);
+        //     return new Response(undefined, {
+        //         status: 301,
+        //         headers: {location: `${HTTPS_PREFIX}${uri}`}
+        //     });
+        // }
 
         // render desired page
         const page = await getPage(ctx.params.name);
